8371adf2e57fd86065062056200e31419719d3c8
bmagee2 <bamagee2@gmail.com>
2020-06-11 12:27:13 -0400

    drop table

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const monologue = require('./controllers/monologuecontroller');
 
 // DATABASE
 const sequelize = require('./db');
-sequelize.sync();
+sequelize.sync({force: true});
 app.use(express.json());
 app.use(require('./middleware/headers'));
 
