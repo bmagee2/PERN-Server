f23e67a78d27359247a0d390c7abac30370753fe
bmagee2 <bamagee2@gmail.com>
2020-06-16 22:35:43 -0400

    server

--- models/user.js
+++ models/user.js
@@ -4,6 +4,14 @@ module.exports = (sequelize, DataTypes) => {
 
     const User = sequelize.define('user', {
 
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
         email: {
             type: DataTypes.STRING,
             required: true,
